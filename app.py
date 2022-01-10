2346a4f8332a0e1a5b631d77c3f9c4dd271218e1
skippyjarro <58606346+skippyjarro@users.noreply.github.com>
2022-01-09 23:14:56 -0600

    heroku files

--- app.py
+++ app.py
@@ -14,29 +14,30 @@ def index():  # put application's code here
     return render_template('index.html')
 
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST', 'GET'])
 def predict():
-    gender = request.form.get('Sex')
-    age = int(request.form.get('Age'))
-    hypertension = int(request.form.get('Hypertension'))
-    heart_disease = int(request.form.get('Heart_Disease'))
-    ever_married = request.form.get('Ever_Married')
-    work_type = request.form.get('Work_Type')
-    residence_type = request.form.get('Residence_type')
-    avg_glucose_level = float(request.form.get('avg_glucose_level'))
-    bmi = int(request.form.get('bmi'))
-    smoking_status = request.form.get('Smoking_Status')
-    features = pd.DataFrame([[gender, age, hypertension, heart_disease, ever_married, work_type, residence_type,
+    if request.method == 'POST':
+        gender = request.form.get('Sex')
+        age = int(request.form.get('Age'))
+        hypertension = int(request.form.get('Hypertension'))
+        heart_disease = int(request.form.get('Heart_Disease'))
+        ever_married = request.form.get('Ever_Married')
+        work_type = request.form.get('Work_Type')
+        residence_type = request.form.get('Residence_type')
+        avg_glucose_level = float(request.form.get('avg_glucose_level'))
+        bmi = int(request.form.get('bmi'))
+        smoking_status = request.form.get('Smoking_Status')
+        features = pd.DataFrame([[gender, age, hypertension, heart_disease, ever_married, work_type, residence_type,
                               avg_glucose_level, bmi, smoking_status]],
                             columns=['gender', 'age', 'hypertension', 'heart_disease', 'ever_married', 'work_type',
                                      'Residence_type', 'avg_glucose_level', 'bmi', 'smoking_status'])
-    prediction = model.predict(features)
-    if prediction == 1:
-        result = 'You are at high risk of Stroke.  Please consult your physician.'
-    else:
-        result = 'Your risk of Stroke is low.'
+        prediction = model.predict(features)
+        if prediction == 1:
+            result = 'You are at high risk of Stroke.  Please consult your physician.'
+        else:
+            result = 'Your risk of Stroke is low.'
 
-    return render_template('index.html', prediction_text=format(result))
+        return render_template('index.html', prediction_text=format(result))
 
 
 if __name__ == '__main__':
